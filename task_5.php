d0067358da6f4d413968a27451de98d6a38b563a
Ashikur_Rahman <ashikurrahman7134@gmail.com>
2023-09-27 11:07:56 +0600

    first commit

--- task_5.php
+++ task_5.php
@@ -1,19 +1,33 @@
-<?php 
+<?php
 
 // Task 5: Password Generator
 
 // Create a PHP function called generatePassword($length) that generates a random password of the specified length. The password should include lowercase letters, uppercase letters, numbers, and special characters (!@#$%^&*()_+). Write a PHP program to generate a password with a length of 12 characters using this function and print the password.
 
 
-function password_generate($chars){
+function generatePassword($length){
+    $uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    $numberChars = '0123456789';
+    $specialChars = '!@#$%^&*()_+';
 
-    $_alphaSmall = 'abcdefghijklmnopqrstuvwxyz';
-    $_alphaCaps  = strtoupper($_alphaSmall);
-    $_numerics   = '1234567890';
-    $_specialChars = '!@#$%^&*()_+';
+    $password = $uppercaseChars[mt_rand(0, strlen($uppercaseChars) - 1)];
+    $password .= $lowercaseChars[mt_rand(0, strlen($lowercaseChars) - 1)];
+    $password .= $numberChars[mt_rand(0, strlen($numberChars) - 1)];
+    $password .= $specialChars[mt_rand(0, strlen($specialChars) - 1)];
 
-    $data = $_alphaSmall . $_alphaCaps . $_numerics . $_specialChars;
-    return substr(str_shuffle($data), 0, $chars);
+    $remainingLength = $length - 4;
+    $allChars = $uppercaseChars . $lowercaseChars . $numberChars . $specialChars;
+
+    for ($i = 0; $i < $remainingLength; $i++) {
+        $password .= $allChars[mt_rand(0, strlen($allChars) - 1)];
+    }
+
+    $password = str_shuffle($password);
+
+    return $password;
 }
 
-echo password_generate(12) . "\n";
+$generatedPassword = generatePassword(12);
+
+echo "Generated Password: " . $generatedPassword;
